4e7528d15a0a9322433d6a6a9ca5723bc6720840
Brandon <53629256+Broadan26@users.noreply.github.com>
2020-10-13 13:42:39 -0700

    Add files via upload

---
+++ BrandonTempConverterForm/TemperatureConverter/ITemperatureConvert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TemperatureConverter
+{
+    [ServiceContract]
+    public interface ITemperatureConvert
+    {
+        [OperationContract]
+        int c2f(int celsius);
+
+        [OperationContract]
+        int f2c(int fahreneheit);
+    }
+}
